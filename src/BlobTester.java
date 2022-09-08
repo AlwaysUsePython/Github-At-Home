4a9ecda905b504f41a0d73d2a7393b58eb53b972
Elliot Lichtman <elliot.joseph.lichtman@gmail.com>
2022-09-08 14:35:25 -0700

    Blob Working

---
+++ src/BlobTester.java
@@ -0,0 +1,17 @@
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+
+public class BlobTester {
+	
+	public static void main (String [] args) throws IOException {
+		
+		System.out.println("Recognizing Edits");
+		
+		Blob blob = new Blob("Test");
+		
+		
+	}
+	
+}
